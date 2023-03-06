bc7a8623faa3e07f67e6873aa285dbbf6802d82b
liupengfei <1608272694@qq.com>
2023-03-07 01:09:58 +0800

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
 import { Button, App } from "antd";
+import homeImage from "../assets/home.svg";
 
 // core的上下文直接从props注入
 export default function Home(props) {
   const { modal } = App.useApp();
   return (
-    <div>
+    <div
+      style={{
+        height: "80vh",
+        backgroundImage: `url(${homeImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <Button
         type="primary"
         onClick={() => {
