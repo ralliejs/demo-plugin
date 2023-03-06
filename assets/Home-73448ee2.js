d0a2b290bd07f9fe1ae6a33ba84294dcdef8bc7e
run-nan <1608272694@qq.com>
2023-03-06 17:10:44 +0000

    Deploying to gh-page from @ ralliejs/demo-plugin@bc7a8623faa3e07f67e6873aa285dbbf6802d82b 🚀

--- assets/Home-73448ee2.js
+++
@@ -1,1 +0,0 @@
-const e=window.React,o=window.Antd.Button,c=window.Antd.App;function l(t){const{modal:n}=c.useApp();return e.createElement("div",null,e.createElement(o,{type:"primary",onClick:()=>{n.info({title:"Hello",content:t.text})}},t.text))}export{l as default};
